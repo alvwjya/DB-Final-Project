4856cc0e57111d1e9119cd7f6292c18820f91cda
davinpchandra <davinpratamachandra@gmail.com>
2020-12-21 17:25:41 +0700

    Seventh Commit Davin

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -3,30 +3,49 @@ package sample;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginController {
     public Button loginButton;
     public TextField usernameField;
     public PasswordField passwordField;
+    Connection connect = new Connection();
 
-    public void loginButton(){
-        try{
-            FXMLLoader loader= new FXMLLoader(getClass().getResource("Home.fxml"));
-            Parent root = loader.load();
+    public void loginButton() throws SQLException {
+        PreparedStatement prepStat = connect.getPrepStat("SELECT * FROM Employee WHERE username = '" + usernameField.getText() + "' AND pass = '" + passwordField.getText() + "';");
+        ResultSet rs = prepStat.executeQuery();
+        if (!rs.next()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Something Wrong!");
+            alert.setHeaderText("Wrong Username or Password!");
+            alert.setContentText("Make sure you input the right info!");
 
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Nu Aneka");
-            stage.show();
-            Stage closeWindow = (Stage) loginButton.getScene().getWindow();
-            closeWindow.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
+            try {
+                FXMLLoader loader= new FXMLLoader(getClass().getResource("Home.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Nu Aneka");
+                stage.show();
+                Stage closeWindow = (Stage) loginButton.getScene().getWindow();
+                closeWindow.close();
+            } catch (IOException e){
+                System.out.println("File not found!");
+            }
         }
+
+
 
     }
 }
